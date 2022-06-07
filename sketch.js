2e69c3066361ebbda3a779253fe0b10ca5c9e910
oscariotes <larrysuico2@gmail.com>
2022-06-07 16:32:52 +0800

    add eraser and random color selector

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 let colorPick = document.getElementById("pickAcolor");
 let myColor;
+let erase = document.getElementById("erase");
+const dColor = document.getElementById("dColor");
+let randomColor = document.getElementById("randomColor");
+let eraseColor;
 let userInput;
 let defaultValue = 20;
 const mainContainer = document.querySelector("mainContainer")
 const container = document.querySelector(".container");
 const warning = document.querySelector(".warning");
-let erase = document.getElementById(".erase");
-
 
 //creates div blocks
 function rowColumn(userInput) {
@@ -16,42 +18,45 @@ function rowColumn(userInput) {
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < (rows * cols); i++) {
     let eachBox = document.createElement("div");
-    //eachBox.innerText = (i + 1);
     container.appendChild(eachBox).className = "box";
  }
 }
 
-
-
 rowColumn(defaultValue);
 
 //Change number of grid
-
   document.getElementById("gridCount").onclick = function gridCount(){
-    let userInput = prompt("type any number to replace number of grid");
+    let userInput = prompt("Type grid number. It should be less than a hundred.");
       if (userInput <= 100) {
         container.replaceChildren();
         warning.replaceChildren();
         rowColumn(userInput);
         color();
       } if (userInput > 100) {
-        //container.replaceChildren();
         let para = document.createElement("p");
-        para.innerText = "Grids exceeds a humdred.";
-        warning.appendChild(para);
-      }
-     
-         
+        alert ("Grids exceeds a hundred. It should be 100 or less");
+      }       
   }
-  
 
 
+let newColor = "#a9a9a9";
 
+dColor.addEventListener("click", function(){
+  newColor = dColor.value;
+}, false)  
+
+erase.addEventListener("click", function(){
+  newColor = erase.value;
+}, false)
 
 colorPick.addEventListener("input", function() {
-  myColor = colorPick.value;
+  newColor = colorPick.value;
 }, false);
- 
+
+randomColor.addEventListener("click", function(){
+  newColor = "#" + ((1<<24)*Math.random() | 0).toString(16);
+}, false)
+
 var mouseDown = false;
 document.body.onmousedown = function() {
 mouseDown = true;
@@ -60,38 +65,22 @@ document.body.onmouseup = function() {
 mouseDown = false;
 };
 
-
-
-
-
 //basic grid block color function
-
 function color (){
-
-
+  
   let eachBox = document.querySelectorAll(".box");
   eachBox.forEach(box =>{
-    box.addEventListener("mouseover", function(){
-      
+    box.addEventListener("mouseover", function(){ 
     })
-    box.addEventListener("mousedown", function(){{
-       box.style.backgroundColor = "gray";
-       box.style.backgroundColor = myColor;
+    box.addEventListener("mousedown", function(){{    
+       box.style.backgroundColor = newColor;      
     }
    })
-  box.addEventListener("mousemove", function(){{
+  box.addEventListener("mousemove", function(){
     if (mouseDown) {
-      box.style.backgroundColor = "gray";
-      box.style.backgroundColor = myColor;
-    }
+    box.style.backgroundColor = newColor; 
   }
    })
  })
 }
-
 color();
-
-
-
-
-
